62306848bfa4a9427e164b67c672506c858870a3
Murilo Honorio <murilo.honorio@usp.br>
2013-07-01 14:08:13 -0300

    Incluída biblioteca WSMO4J

--- src/fassw/Main.java
+++ src/fassw/Main.java
@@ -1,6 +1,7 @@
 package fassw;
 
 import java.io.IOException;
+import javax.swing.JOptionPane;
 import org.wsmo.common.exception.InvalidModelException;
 
 /**
@@ -13,12 +14,15 @@ public class Main {
 
     /**
      * Método de entrada da ferramenta.
+     * Caso passe como parametro "mapear entrada.wsdl saida.wsml", funciona em linha de comando.
+     * Senao, funciona atraves da interface grafica.
+     * 
      * @param args nome da operacao, arquivo de entrada, arquivo de saida
      */
     public static void main(String[] args) throws UnsupportedOperationException, InvalidModelException, IOException {
         boolean sucesso;
         
-        if (args[0].equals("mapear")) {
+        if (args.length == 3 && args[0].equals("mapear")) {
             Mapeador mapeador = new Mapeador(args[1], args[2]);
             sucesso = mapeador.executar();
             if (sucesso) {
@@ -26,6 +30,16 @@ public class Main {
             }
             else {
                 System.err.println("Módulo mapeador não conseguiu concluir a tarefa.");
+            }
+        }
+        else {
+            Mapeador mapeador = new Mapeador();
+            sucesso = mapeador.executar();
+            if (sucesso) {
+                JOptionPane.showMessageDialog(null, "Módulo mapeador efetuou a tarefa com sucesso.");
+            }
+            else {
+                JOptionPane.showMessageDialog(null, "Módulo mapeador não conseguiu concluir a tarefa.");
             }
         }
     }
